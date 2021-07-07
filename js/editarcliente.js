74872449d16151a45113812985b68c6901617721
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-07-06 22:32:54 -0500

    Query para obtener cada cliente desde la base de datos

---
+++ js/editarcliente.js
@@ -0,0 +1,51 @@
+(function() {
+
+    let DB;
+    const nombreInput = document.querySelector('#nombre');
+
+    document.addEventListener('DOMContentLoaded', () => {
+        // Verificar el ID de la URL
+        const parametrosURL = new URLSearchParams(window.location.search);
+        const idCliente = parametrosURL.get('id');
+        if(idCliente) {
+            setTimeout(() => { obtenerCliente(idCliente) }, 1000);
+        }
+    });
+
+    function obtenerCliente(id) {
+        const transaction = DB.transaction(['crm'], 'readonly');
+        const objectStore = transaction.objectStore('crm');
+
+
+        const cliente = objectStore.openCursor();
+        cliente.onsuccess = function(e) {
+            const cursor = e.target.result;
+            if (cursor) {
+                console.log(cursor.value);
+                if(cursor.value.id === Number(id)) {
+                    llenarFormulario(cursor.value)
+                }
+                cursor.continue();
+            }
+        }
+    }
+
+    function llenarFormulario(datosCliente) {
+        const {nombre} = datosCliente;
+
+        nombreInput.value = nombre;
+    }
+
+    function conectarDB() {
+        const abrirConexion = window.indexedDB.open('crm', 1);
+
+        abrirConexion.onerror = function() {
+            console.log('Error en conexion para edicion');
+        }
+
+        abrirConexion.onsuccess = function() {
+            DB = abrirConexion.result;
+        }
+    }
+
+})();
